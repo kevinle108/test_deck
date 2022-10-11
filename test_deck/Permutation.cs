11618afe51068065195404772ac76c65935b6b58
Kevin Le <kevinle108@gmail.com>
2022-10-11 14:14:30 -0400

    Add project files.

---
+++ test_deck/Permutation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_deck
+{
+    internal class Permutation
+    {
+        public Permutation(int n)
+        {
+            var src = "";
+            for (int i = 1; i <= n; i++)
+            {
+                src += i;
+            }
+            GeneratePermutations("", src);
+        }
+
+        public List<int> First()
+        {
+            // TODO
+            return new List<int>();
+        }
+
+        public List<int> Next()
+        {
+            // TODO
+            return new List<int>();
+        }
+
+        void GeneratePermutations(string result, string src)
+        {
+            if (src.Length <= 1)
+            {
+                result += src;
+                src = "";
+                Console.WriteLine(result);
+                return;
+            }
+
+            foreach (char c in src)
+            {
+                GeneratePermutations(result + c, src.Trim(c)); ;
+            }
+        }
+
+        public void PrintPermutations(List<string> permutations)
+        {
+            Console.WriteLine("[ ");
+            foreach (var item in permutations)
+            {
+                string line = "";
+                foreach (var number in item)
+                {
+                    line += $"{number} ";
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ test_deck/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using test_deck;
+
+Console.WriteLine("Hello, World!");
+var test = new Permutation(3);
+
